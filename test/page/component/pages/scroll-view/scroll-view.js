be7bc3553fe50fcc1eeff38db0b857d0845dcca5
riki_tree <18624913171@163.com>
2019-11-15 15:17:25 +0800

    日常敲代码

--- test/page/component/pages/scroll-view/scroll-view.js
+++ test/page/component/pages/scroll-view/scroll-view.js
@@ -1,97 +1,103 @@
-// page/component/index.js
+const order = ['demo1','demo2','demo3']
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list:[
-      {
-        id:'view',
-        name:'视图容器',
-        open:false,
-        pages: ['view','scroll-view']
-      }
-    ]
+    toView:'green'
   },
 
-  kindToggle(e){
+upper(e)
+{
+  console.log(e)
+},
+lower(e){
+  console.log(e)
+},
+  scroll(e)
+  {
+    console.log(e)
+  },
+  scrollToTop()
+  {
+    this.setAction({scrollTop:0})
+  },
 
-    const id = e.currentTarget.id
-    const list = this.data.list
-    for(let i = 0,len = list.length;i < len;i++){
-      if(list[i].id === id){
-        list[i].open = !list[i].open
-      }else{
-        list[i].open = false
+  tap(){
+    for(let i = 0;i < order.length;++i){
+      if(order[i] === this.data.toView){
+        this.setData({
+          toView:order[i+1],
+          scrollTop:(i+1)*200
+        })
+        break
       }
     }
-
-    this.setData(
-      {
-        list
-      }
-    )
-
-    wx.reportAnalytics("click_view_programmatically", {})
   },
 
+  tapMove(){
+    this.setData({
+      scrollTop:this.data.scrollTop +10
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    wx.reportAnalytics("enter_home_programmatically", {})
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    return {
-      title:'小程序官方组件展示',
-      path:'page/component/index'
-    }
+      return{
+        title:'scroll-view',
+        path:'page/component/pages/scroll-view/scroll-view'
+      }
   }
 })
